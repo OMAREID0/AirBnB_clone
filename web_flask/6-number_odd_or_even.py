614b69c9a2021184e30a3684f9267a8d4dd0f5fd
OMAREID0 <omareid221b@gmail.com>
2024-03-23 15:13:42 +0200

    fix

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -37,5 +37,6 @@ def number_temblate(n):
 def odd_or_even(n):
     """ display a HTML page only if n is an integer """
     return render_template('6-number_odd_or_even.html', value=n)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
