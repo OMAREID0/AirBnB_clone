1d0d471593848927b45d2e57ec9166c0a23c272e
OMAREID0 <omareid221b@gmail.com>
2024-03-23 04:27:25 +0200

    ana sam3 el 7aky fe asmy kter

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application Python is Cool"""
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_nbnb():
+    """ Prints a Message when / is called """
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def HBNB():
+    """ Prints a Message when /hbnb is called """
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Prints a Message when /c is called """
+    return "C " + text.replace('_', '')
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """ Prints a Message when /python is called """
+    return "Python " + text.replace('_', '')
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def n_number(n):
+    """ Prints a Message when /number is called only if n is an int"""
+    return "{:d} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_temblate(n):
+    """ Prints a Message when /number is called only if n is an int"""
+    return render_template('5-number.html', Number=n)
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def odd_or_even(n):
+    """ display a HTML page only if n is an integer """
+    return render_template('6-number_odd_or_even.html', value=n)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
